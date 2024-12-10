1c7f4fae5d53386b7ad3174fdbbf672e5d05ac0f
mqarmout <majedamf@gmail.com>
2024-12-09 20:59:31 -0500

    finished docker implementation

--- backend/app.py
+++ backend/app.py
@@ -46,4 +46,4 @@ def download_file(name):
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(host="127.0.0.1", port=11000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
